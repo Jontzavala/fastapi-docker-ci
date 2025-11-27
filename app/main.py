53df45d13855a26b5883ee377e799415229ed864
Jonathan Zavala <70041382+Jontzavala@users.noreply.github.com>
2025-11-26 20:58:28 -0600

    pass env vars to container securely

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+APP_ENV = os.getenv("APP_ENV", "development")
+
+@app.get("/env")
+def get_env():
+    return {"environment": APP_ENV}
+
 @app.get("/")
 def read_root():
     return {"message": "Hello from inside a Docker container!!"}
